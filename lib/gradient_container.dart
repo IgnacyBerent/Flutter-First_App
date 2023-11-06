fd56b0f0e5236655a3d74f92a5e5ca2386fd51cf
IgnacyBerent <2gb02ignac@gmail.com>
2023-11-06 23:39:36 +0100

    dart basics

---
+++ lib/gradient_container.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText("Hello, World!"),
+      ),
+    );
+  }
+}
